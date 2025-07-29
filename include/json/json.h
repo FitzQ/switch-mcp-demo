b05189bbd26d3bc0c410b9691044b835a5a5c5f3
FitzQ <30992599+FitzQ@users.noreply.github.com>
2025-07-29 23:56:10 +0800

    Add placeholder for jsoncpp header

---
+++ include/json/json.h
@@ -0,0 +1,3 @@
+// This is a placeholder for the jsoncpp header file.
+// In the actual Switch development environment, this file would be provided by the jsoncpp library.
+// The Makefile is configured to link against the jsoncpp library installed via (dkp-)pacman.
